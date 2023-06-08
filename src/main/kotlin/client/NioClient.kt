2b3532db48d24e3a481c6e572581557213f9aec9
janghyoseog <bestdevhyo1225@gmail.com>
2023-06-09 00:11:10 +0900

    feat: Java NIO 기반의 클라이언트 - 서버 구현

---
+++ src/main/kotlin/client/NioClient.kt
@@ -0,0 +1,59 @@
+package client
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.InetSocketAddress
+import java.net.SocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.SocketChannel
+
+const val hostname = "localhost"
+const val port: Int = 3000
+
+fun main() {
+    println("Start nio client...")
+
+    val inetSocketAddress = InetSocketAddress(hostname, port)
+
+    // SocketChannel 생성
+    val socketChannel: SocketChannel = SocketChannel.open(inetSocketAddress)
+    val localAddress: SocketAddress = socketChannel.localAddress
+    val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
+
+    println("Connecting to nio server on port ${port}...")
+
+    try {
+        while (true) {
+            println("---------------------")
+            print("[${localAddress}] Send message to server: ")
+
+            val message: String = bufferedReader.readLine()
+            if (message in arrayOf("q", "quit", "e", "exit")) {
+                break
+            }
+            val byteArrayMessage: ByteArray = message.encodeToByteArray()
+            val byteBufferMessage: ByteBuffer = ByteBuffer.wrap(byteArrayMessage)
+
+            // 서버에 메시지 전송
+            socketChannel.write(byteBufferMessage)
+            byteBufferMessage.clear()
+
+            // 서버로부터 메시지 수신
+            val receiveMessageSize: Int = socketChannel.read(byteBufferMessage)
+            val receiveMessage: String = String(byteBufferMessage.array()).trim()
+            byteBufferMessage.clear()
+
+            println("[${localAddress}] Receive message size from server: $receiveMessageSize")
+            println("[${localAddress}] Receive message from server: $receiveMessage")
+        }
+
+        println("Exit nio client...")
+    } catch (exception: Exception) {
+        println("exception: ${exception.localizedMessage}")
+    } finally {
+        bufferedReader.close()
+        socketChannel.close()
+
+        println("Close resources...")
+    }
+}
---
+++ src/main/kotlin/server/NioServer.kt
@@ -0,0 +1,107 @@
+package server
+
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.net.SocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.SelectionKey
+import java.nio.channels.Selector
+import java.nio.channels.ServerSocketChannel
+import java.nio.channels.SocketChannel
+
+const val hostname = "localhost"
+const val port: Int = 3000
+
+fun main() {
+    println("Start nio server...")
+
+    // Selector 생성
+    val selector: Selector = Selector.open()
+
+    // ServerSocketChannel 생성
+    // TCP 연결을 수신 대기하는 채널이다.
+    val serverSocketChannel: ServerSocketChannel = ServerSocketChannel.open()
+    serverSocketChannel.configureBlocking(false) // Non-Blocking
+    serverSocketChannel.bind(InetSocketAddress(hostname, port))
+    serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT)
+
+    while (true) {
+        // accept, connect, read, write 이벤트에 대한 준비되어 있는 채널 수를 반환한다.
+        // select() 메서드는 하나 이상의 채널이 준비될 때까지 Block 된다.
+        val eventReadyChannelCount: Int = selector.select()
+
+        println("---------------------")
+        println("[${serverSocketChannel.localAddress}] Count of channels ready for the event: $eventReadyChannelCount")
+
+        val selectedKeys: Set<SelectionKey> = selector.selectedKeys()
+        val selectionKeyIterator: MutableIterator<SelectionKey> =
+            selectedKeys.iterator() as MutableIterator<SelectionKey>
+
+        // 준비된 채널이 존재하는 경우
+        while (selectionKeyIterator.hasNext()) {
+
+            val selectionKey: SelectionKey = selectionKeyIterator.next()
+
+            if (selectionKey.isAcceptable) {
+                registerSocketChannel(serverSocketChannel = serverSocketChannel, selector = selector)
+            }
+
+            if (selectionKey.isReadable) {
+                readAndWrite(selectionKey = selectionKey)
+            }
+
+            if (selectionKey.isWritable) {
+                println("isWritable...")
+            }
+
+            selectionKeyIterator.remove()
+        }
+    }
+}
+
+fun registerSocketChannel(serverSocketChannel: ServerSocketChannel, selector: Selector) {
+    try {
+        val socketChannel: SocketChannel? = serverSocketChannel.accept()
+
+        socketChannel?.run {
+            socketChannel.configureBlocking(false) // Non-Blocking
+            socketChannel.register(selector, SelectionKey.OP_READ)
+
+            println("[${socketChannel.remoteAddress}] New client connected...")
+        }
+    } catch (exception: Exception) {
+        println("[registerSocketChannel] exception: ${exception.localizedMessage}")
+    }
+}
+
+fun readAndWrite(selectionKey: SelectionKey) {
+    try {
+        // Buffer 생성
+        val byteBuffer: ByteBuffer = ByteBuffer.allocate(256)
+        // read 이벤트에 대한 채널
+        val socketChannel: SocketChannel = selectionKey.channel() as SocketChannel
+        val remoteAddress: SocketAddress = socketChannel.remoteAddress
+        // 버퍼에서 채널로 데이터 읽기
+        val receiveMessageSize: Int = socketChannel.read(byteBuffer)
+        val receiveMessage: String = String(byteBuffer.array()).trim()
+
+        println("[${remoteAddress}] Receive message size from client: $receiveMessageSize")
+        println("[${remoteAddress}] Receive message from client: $receiveMessage")
+
+        if (receiveMessageSize == -1) {
+            // 채널 종료
+            socketChannel.close()
+            byteBuffer.clear()
+
+            println("[${remoteAddress}] Not accepting client messages anymore...")
+        } else {
+            byteBuffer.flip() // 버퍼를 '읽기' 모드에서 '쓰기' 모드로 전환한다.
+            socketChannel.write(byteBuffer) // 클라이언트로 메시지 전송
+            byteBuffer.clear()
+
+            println("[${remoteAddress}] Send message to client: $receiveMessage")
+        }
+    } catch (exception: Exception) {
+        println("[readAndWrite] exception: ${exception.localizedMessage}")
+    }
+}
